2fbf26c0c5170213add81cbe53d2ac012d08035b
BagasPrimada <55829746+BagasPrimada@users.noreply.github.com>
2019-10-26 07:28:52 +0700

    upload studi kasus 01

---
+++ 2120180140-K01-PBO-TI20192020-3-4-01/2120180140-K01-PBO-TI20192020-3-4-02.java
@@ -0,0 +1,35 @@
+package mahasiswa;
+public class Mahasiswa1 {
+    public static void main(String[] args) {
+    mahasiswa mahasiswa_1= new mahasiswa();
+    mahasiswa_1.alamat="Sukeroho Bojonegoro";
+    mahasiswa_1.nama="Arini1 Mailiyal Haq";
+    mahasiswa_1.nim="2120180137";
+    mahasiswa_1.biodata(); 
+    
+    mahasiswa mahasiswa_2= new mahasiswa();
+    mahasiswa_2.alamat="campurejo Bojonegoro";
+    mahasiswa_2.nama="budi sasono";
+    mahasiswa_2.nim="2120180138";
+    mahasiswa_2.biodata();
+    
+    mahasiswa mahasiswa_3= new mahasiswa();
+    mahasiswa_3.alamat="pacul Bojonegoro";
+    mahasiswa_3.nama="budi anduk";
+    mahasiswa_3.nim="2120180139";
+    mahasiswa_3.biodata();
+    
+    mahasiswa mahasiswa_4= new mahasiswa();
+    mahasiswa_4.alamat="kalitidu";
+    mahasiswa_4.nama="andika";
+    mahasiswa_4.nim="2120180131";
+    mahasiswa_4.biodata();
+    
+    mahasiswa mahasiswa_5= new mahasiswa();
+    mahasiswa_5.alamat="dander bojonegoro";
+    mahasiswa_5.nama="susi susanti";
+    mahasiswa_5.nim="2120180132";
+    mahasiswa_5.biodata();
+    }
+    
+}
